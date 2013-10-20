b20253e8c6b280b912ea41a947cedd5a1509838b
Lunatrius <jadran.kotnik@gmail.com>
2013-10-20 03:07:20 +0200

    Added an example file to run the thing.

---
+++ example.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import genbbcode
+# mcfdata can be ommited, but I preffer to have that data on git,
+# leaving out only the username and password
+import mcfdata
+
+
+def main():
+    username = 'username'
+    password = 'password'
+    # read the comment in import section about mcfdata
+    threads = mcfdata.threads('path/to/change/logs')
+
+    genbbcode.genbbcode(username, password, threads)
+
+
+if __name__ == '__main__':
+    main()
