4848c6f78d4a722b1bf6aba3818724c22a365748
Lunatrius <jadran.kotnik@gmail.com>
2013-10-20 02:46:04 +0200

    Initial commit.

---
+++ genbbcode.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+import os
+import re
+import sys
+import subprocess
+from argparse import ArgumentParser
+from pprint import pprint
+import mechanize
+
+import urllib
+import urllib2
+import cookielib
+import hashlib
+import time
+
+
+# update the thread
+def genbbcode(username, password, threads=[]):
+    # create a new browser
+    br = mechanize.Browser()
+    br.set_handle_robots(False)
+
+    # open login form
+    br.open('http://www.minecraftforum.net/index.php?app=curseauth&module=global&section=login')
+
+    # select the form and fill in the username/password
+    br.select_form(nr=0)
+    br.form['ips_username'] = username
+    br.form['ips_password'] = password
+
+    # submit the form
+    br.submit()
+
+    # for each thread
+    for thread in threads:
+        continue
+
+        print 'Updating %s [%d]...' % (thread.get('title', 0), thread.get('t', 0))
+        # open the thread edit page
+        br.open('http://www.minecraftforum.net/index.php?app=forums&module=post&section=post&do=edit_post&f=%d&t=%d&p=%d&st=' % (thread.get('f', 0), thread.get('t', 0), thread.get('p', 0)))
+
+        # print the title of the window, just to make sure
+        print '  Title: "%s"' % br.title()
+
+        # inital form id
+        frmid = 0
+
+        # find the postingform id
+        for frm in br.forms():
+            if frm.attrs['id'] == 'postingform':
+                break
+            frmid += 1
+
+        # select the form and fill in the information
+        br.select_form(nr=frmid)
+        tag = thread.get('tag', None)
+        title = thread.get('title', '')
+        br.form['TopicTitle'] = '[%s] %s' % (tag, title) if tag and len(tag) > 0 else title
+        br.form['ipsTags'] = ','.join(thread.get('tags', []))
+
+        try:
+            with open(thread.get('file', None), 'r') as fh:
+                content = ''.join(fh.readlines())
+
+                for key, value in thread.get('replacement', {}).items():
+                    content = content.replace('%%%s%%' % (key), value)
+
+                br.form['Post'] = content
+        except BaseException as e:
+            print 'ERROR! %s' % (e)
+
+        # submit the form
+        br.submit()
+
+    p = subprocess.Popen(['git', 'status'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    data = p.communicate()
+    stdout = data[0]
+
+    # attempt to commit the changes
+    if stdout.find('nothing to commit, working directory clean') == -1:
+        print stdout
+        if query_input('Would you like to commit the changes?', True) == True:
+            p = subprocess.Popen(['git', 'add', '.'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            data = p.communicate()
+
+            p = subprocess.Popen(['git', 'diff', '--cached'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            data = p.communicate()
+            stdout = data[0]
+
+            print stdout
+
+            if query_input('Would you really like to commit the changes?', None) == True:
+                p = subprocess.Popen(['git', 'commit', '-m', '"Updated assets at %s"' % (time.strftime('%d.%m.%Y, %H:%M:%S'))], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                data = p.communicate()
+
+                p = subprocess.Popen(['git', 'push'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                data = p.communicate()
+                stdout = data[0]
+                print stdout
+
+
+# Modified version of: http://stackoverflow.com/questions/3041986/#3041990
+def query_input(question, default=None):
+    valid = {
+        True: True,
+        "true": True,
+        "yes": True,
+        "ye": True,
+        "y": True,
+        False: False,
+        "false": False,
+        "no": False,
+        "n": False
+    }
+
+    if default == None:
+        prompt = " [y/n] "
+    elif default == True:
+        prompt = " [Y/n] "
+    elif default == False:
+        prompt = " [y/N] "
+    else:
+        raise ValueError("Invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = raw_input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
---
+++ mcfdata.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+import os
+import re
+
+
+def threads(base):
+    return [
+        {
+            'file': '1296949-misc.bbcode',
+            'f': 51,
+            't': 1296949,
+            'p': 15841019,
+            'title': 'Lunatrius\' mods',
+            'tag': '1.6.4',
+            'tags': [
+                'forge',
+                'monster spawn highlighter',
+                'stackie',
+                'ingame info xml',
+                'ingame info',
+                'light level'
+            ],
+            'replacement': {
+                'changelogigi': clean_changelog(base, 'ingameinfo'),
+                'changelogmsg': clean_changelog(base, 'monsterspawnhighlighter'),
+                'changelogstackie': clean_changelog(base, 'stackie')
+            }
+        },
+        {
+            'file': '1468779-schematica.bbcode',
+            'f': 51,
+            't': 1468779,
+            'p': 17870619,
+            'title': 'Schematica',
+            'tag': '1.6.4',
+            'tags': [
+                'forge',
+                'schematica',
+                'schematic'
+            ],
+            'replacement': {
+                'changelog': clean_changelog(base, 'schematica')
+            }
+        }
+    ]
+
+
+def clean_changelog(base, modid):
+    with open(os.path.join(base, modid, 'changelog.txt')) as fh:
+        changelog = ''.join(fh.readlines())
+    changelog = re.sub('jenkins-[a-zA-Z0-9]+-(?=\d)', '#', changelog)
+    changelog = changelog[:2048]
+    changelog = changelog[:changelog.rfind('\n\n')]
+    return changelog.strip()
